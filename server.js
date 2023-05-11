9addc9fce79df2fee3b3ce40c2e7d41863137a43
MelissaJesmin <melissa.jesmin25@gmail.com>
2023-05-11 15:14:21 -0400

    DevOps

--- server.js
+++ server.js
@@ -1,7 +1,66 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const path = require('path')
+const cors = require('cors')
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.json())
+app.use(cors())
 
-app.listen(4000, () => console.log(`server running on 4000`))
+const {ROLLBAR_ACCESS_TOKEN} = process.env
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: `${ROLLBAR_ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+const foods = ['Pizza', 'Burger', 'Cake']
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/foods', (req, res) => {
+    rollbar.info('someone requested the list of foods')
+    res.status(200).send(foods)
+})
+
+app.post('/api/foods', (req, res) => {
+   let {name} = req.body
+
+   const index = foods.findIndex(food => {
+       return food === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           foods.push(name)
+           res.status(200).send(foods)
+       } 
+       
+       else if (name === ''){
+           res.status(400).send('You must enter a name.')
+       } 
+       else {
+           res.status(400).send('That food already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/foods/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    foods.splice(targetIndex, 1)
+    res.status(200).send(foods)
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
